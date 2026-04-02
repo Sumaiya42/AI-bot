4cfb43449b61a4cbab1fbd82d4992ffa2c53e278
Sumaiya Tabassum <148692569+Sumaiya42@users.noreply.github.com>
2026-04-02 13:03:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain_core.memory import ConversationBufferMemory
 
 
 # -------------------------------------------------------------------
